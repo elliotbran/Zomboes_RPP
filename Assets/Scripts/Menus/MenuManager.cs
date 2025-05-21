0b6f39f25d602048086e633a21609e42fffc867a
dankie94 <cpa.a4336@cpasalduie.com>
2025-05-21 20:31:16 +0200

    Menus
    21/05/25

---
+++ Assets/Scripts/Menus/MenuManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject introScreen;
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        introScreen.SetActive(true);
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        IntroScreen();
+    }
+
+    void IntroScreen()
+    {
+        if (introScreen == true)
+        {
+            if (Input.anyKeyDown)
+            {
+                introScreen.SetActive(false);
+                mainMenu.SetActive(true);
+            }
+        }
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void Exit()
+    {
+        mainMenu.SetActive(false);
+        introScreen.SetActive(true);
+    }
+
+    public void Options()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    public void Back()
+    {
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+}
