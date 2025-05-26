85f9884c988d888f109cdc1ca99e3581996117ad
dankie94 <cpa.a4336@cpasalduie.com>
2025-05-26 20:05:06 +0200

    rango de deteccion del enemigo al jugador y persecucion

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,12 +8,40 @@ public class EnemyController : MonoBehaviour
 {
     public Transform objective;
     public float velocity;
-    public NavMeshAgent IA;
+    public NavMeshAgent iA;
+    public float range;
+    public float distance;
+    public bool persiguiendo;
+
+    public int life = 100;
 
     // Update is called once per frame
     void Update()
     {
-        IA.speed = velocity;
-        IA.SetDestination(objective.position);
+        distance = Vector3.Distance(iA.transform.position, objective.position);
+        if (distance < range)
+        {
+            persiguiendo = true;
+        }
+        else if (distance > range + 3)
+        {
+            persiguiendo = false;
+        }
+
+        if (persiguiendo == false)
+        {
+            iA.speed = 0;
+        }
+        else if (persiguiendo == true)
+        {
+            iA.speed = velocity;
+            iA.SetDestination(objective.position);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(iA.transform.position, range);
     }
 }
