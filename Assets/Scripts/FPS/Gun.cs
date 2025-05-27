3e5346c994a7bd9ce58aed4c410de7283a619be9
electrojorge <jorge.larrocha@gmail.com>
2025-05-27 17:58:06 +0200

    recargar arma

--- Assets/Scripts/FPS/Gun.cs
+++ Assets/Scripts/FPS/Gun.cs
@@ -10,18 +10,31 @@ public class Gun : MonoBehaviour
     public float fireRate = 15f;
     public float impactForce = 30f;
 
+    public int defaultMagazine;
+    public int actualMagazine;
+
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
     public GameObject impactEffect;
 
     private float nextTimeToFire = 0f;
-    // Update is called once per frame
+
+    void Start()
+    {
+        actualMagazine = defaultMagazine;
+    }
     void Update()
     {
-        if (Input.GetButtonDown("Fire1") && Time.time >= nextTimeToFire)//Si queremos hacer un arma automatica solo hay que quitar el "Down" de "GetButtonDown"
+        if (Input.GetButtonDown("Fire1") && Time.time >= nextTimeToFire && actualMagazine > 0)//Si queremos hacer un arma automatica solo hay que quitar el "Down" de "GetButtonDown"
         {
             nextTimeToFire = Time.time + 1f/fireRate;
             Shoot();
+            actualMagazine--;
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Reload();
         }
     }
 
@@ -32,6 +45,7 @@ public class Gun : MonoBehaviour
         if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name);
+            Debug.Log("Disparo");
 
             Target target = hit.transform.GetComponent<Target>();
             if (target != null)
@@ -47,5 +61,16 @@ public class Gun : MonoBehaviour
             //GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal)); //falta hacer la animacion de disparo
             //Destroy(impactGO, 2f);
         }
+    }
+
+    void Reload()
+    {
+        if(actualMagazine != defaultMagazine)
+        {
+            int diff = defaultMagazine - actualMagazine;
+            actualMagazine += diff;
+            Debug.Log("recargando " + diff.ToString() + " balas");
+        }
+        
     }
 }
