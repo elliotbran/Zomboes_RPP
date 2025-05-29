2e767e3b2e1a3ce37b5d8467d52a093b27ad8f16
dankie94 <cpa.a4336@cpasalduie.com>
2025-05-29 20:04:53 +0200

    Revert "rango de deteccion del enemigo al jugador y persecucion"
    This reverts commit 85f9884c988d888f109cdc1ca99e3581996117ad.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -8,40 +7,12 @@ public class EnemyController : MonoBehaviour
 {
     public Transform objective;
     public float velocity;
-    public NavMeshAgent iA;
-    public float range;
-    public float distance;
-    public bool persiguiendo;
-
-    public int life = 100;
+    public NavMeshAgent IA;
 
     // Update is called once per frame
     void Update()
     {
-        distance = Vector3.Distance(iA.transform.position, objective.position);
-        if (distance < range)
-        {
-            persiguiendo = true;
-        }
-        else if (distance > range + 3)
-        {
-            persiguiendo = false;
-        }
-
-        if (persiguiendo == false)
-        {
-            iA.speed = 0;
-        }
-        else if (persiguiendo == true)
-        {
-            iA.speed = velocity;
-            iA.SetDestination(objective.position);
-        }
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(iA.transform.position, range);
+        IA.speed = velocity;
+        IA.SetDestination(objective.position);
     }
 }
